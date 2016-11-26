a71666086970527d120b33bf3517f943f0d86555
kaushik vinay <kaushik@recroup.com>
2016-11-26 03:59:51 -0500

    changed assets path

--- lib/ikon.rb
+++ lib/ikon.rb
@@ -2,6 +2,6 @@ require "ikon/version"
 
 module Ikon
   class Engine < ::Rails::Engine
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.paths << File.expand_path("../app/assets/fonts", __FILE__)
   end
 end
